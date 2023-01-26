ea68a5bf4bd9e854a115eef4c67c2b49c519e404
Damian Tarnawsky <damian@ionic.io>
2023-01-26 12:54:10 -0800

    Initial Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { Geolocation, Position } from '@capacitor/geolocation';
+
+let that: HomePage;
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,43 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  label = '';
+  watching = false;
+  count = 0;
+  id = '';
+  constructor(private ngZone: NgZone) {
+    that = this;
+  }
+
+  async get() {
+    try {
+      const coordinates = await Geolocation.getCurrentPosition();
+      this.label = JSON.stringify(coordinates);
+    } catch (err) {
+      this.label = err as string;
+    }
+  }
+
+  async watch() {
+    this.id = await Geolocation.watchPosition({ enableHighAccuracy: true }, this.onPosition);
+    this.watching = true;
+  }
+
+  onPosition(position: Position | any) {
+    that.ngZone.run(() => {
+      console.log(position);
+      that.count++;
+      if (position?.coords) {
+        that.label = JSON.stringify(position);
+      } else {
+        that.label = `Error: ${position}`;
+      }
+    });
+  }
+
+  async stop() {
+    await Geolocation.clearWatch({ id: this.id });
+    this.watching = false;
+  }
 
 }
